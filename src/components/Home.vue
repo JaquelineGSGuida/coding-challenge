57c85104afe10b3aa6471e092e934d58c87baa52
JaquelineGSGuida <77866561+JaquelineGSGuida@users.noreply.github.com>
2025-05-12 20:16:43 -0300

    Transferred key to env variable, added pagination and error handling

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,7 +1,94 @@
-<script setup></script>
+<script setup>
+import { ref } from 'vue'
+import axios from 'axios'
+
+const query = ref('')
+const recipes = ref([])
+const error = ref(null)
+const isLoading = ref(false)
+
+const recipesPerPage = 5
+let currentPage = ref(1)
+let totalPages = ref(1)
+
+/**
+ * This calls the API and fetches the recipes already considering the limit
+ * of recipes per page
+ * @param page - used to calculate the offset in the API call
+ */
+const fetchRecipes = async (page = 1) => {
+  error.value = null
+  isLoading.value = true
+
+  try {
+    const apiKey = import.meta.env.VITE_SPOONACULAR_API_KEY
+
+    const response = await axios.get(`https://api.spoonacular.com/recipes/complexSearch`, {
+      params: {
+        apiKey,
+        query: query.value,
+        number: recipesPerPage,
+        offset: (page - 1) * recipesPerPage,
+      },
+    })
+
+    recipes.value = response.data.results
+    totalPages.value = Math.ceil(response.data.totalResults / recipesPerPage)
+  } catch (err) {
+    error.value = 'Error fetching recipes.'
+    console.error('Error fetching recipes: ', err)
+  } finally {
+    isLoading.value = false
+  }
+}
+
+/**
+ * This reset the currentPage value and calls the fetchRecipes function
+ */
+const searchRecipes = () => {
+  currentPage.value = 1
+  fetchRecipes(currentPage.value)
+}
+
+/**
+ * This changes the current page and calls the fetchRecipes function again
+ * @param page - the page number to be changed to
+ */
+const changePage = (page) => {
+  if (page > 0 && page <= totalPages.value) {
+    currentPage.value = page
+    fetchRecipes(currentPage.value)
+  }
+}
+</script>
 
 <template>
   <h1>Welcome! Search for a recipe</h1>
+  <div class="search-area">
+    <input v-model="query" type="text" name="search" placeholder="Search recipe" />
+    <button @click="searchRecipes" :disabled="isLoading">Search</button>
+  </div>
+
+  <div v-if="loading" class="loading">Loading...</div>
+  <div v-if="error" class="error">{{ error }}</div>
+
+  <div v-if="recipes.length > 0" class="recipes">
+    <div v-for="recipe in recipes" :key="recipe.id">
+      <img :src="recipe.image" alt="" />
+      {{ recipe.title }}
+    </div>
+
+    <div class="pagination">
+      <button @click="changePage(currentPage - 1)" :disabled="currentPage === 1">Previous</button>
+      <span>Page {{ currentPage }} of {{ totalPages }}</span>
+      <button @click="changePage(currentPage + 1)" :disabled="currentPage === totalPages">
+        Next
+      </button>
+    </div>
+  </div>
+  <div v-else>
+    <span>No recipes found.</span>
+  </div>
 </template>
 
 <style></style>
