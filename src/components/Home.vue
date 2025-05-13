0ba0f240f7324f4744d58fee73f8114cb312d4e5
JaquelineGSGuida <77866561+JaquelineGSGuida@users.noreply.github.com>
2025-05-13 19:48:47 -0300

    Fixed main layout and design for the homepage

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -10,8 +10,9 @@ const error = ref(null)
 const isLoading = ref(false)
 
 const recipesPerPage = 5
-let currentPage = ref(1)
-let totalPages = ref(1)
+const currentPage = ref(1)
+const totalPages = ref(1)
+const hasSearched = ref(false)
 
 const selectedCuisine = ref('')
 
@@ -40,7 +41,7 @@ const fetchRecipes = async (page = 1) => {
     recipes.value = response.data.results
     totalPages.value = Math.ceil(response.data.totalResults / recipesPerPage)
   } catch (err) {
-    error.value = 'Error fetching recipes.'
+    error.value = 'Error fetching recipes. Please try again.'
     console.error('Error fetching recipes: ', err)
   } finally {
     isLoading.value = false
@@ -52,6 +53,7 @@ const fetchRecipes = async (page = 1) => {
  */
 const searchRecipes = () => {
   currentPage.value = 1
+  hasSearched.value = true
   fetchRecipes(currentPage.value)
 }
 
@@ -68,42 +70,119 @@ const changePage = (page) => {
 </script>
 
 <template>
-  <h1>Welcome! Search for a recipe</h1>
-  <div class="search-area">
-    <select v-model="selectedCuisine">
-      <option value="">All Cuisines</option>
-      <option v-for="cuisine in cuisines" :key="cuisine" :value="cuisine">{{ cuisine }}</option>
-    </select>
-    <p v-if="selectedCuisine">Filtering for: {{ selectedCuisine }}</p>
-    <input
-      v-model="query"
-      type="text"
-      name="search"
-      placeholder="Search recipe"
-      @keyup.enter="searchRecipes"
-    />
-    <button @click="searchRecipes" :disabled="isLoading">Search</button>
-  </div>
+  <div class="min-vh-100 d-flex justify-content-center py-5 px-3 px-md-5">
+    <div class="bg-white p-4 mx-auto shadow w-100" style="max-width: 1140px">
+      <h1 class="font-cursive display-4 display-md-3 text-center my-4">
+        Welcome! Search for a recipe:
+      </h1>
+      <div class="row gy-3 align-items-center mb-4">
+        <div class="col-12 col-md-6">
+          <div class="dropdown w-100">
+            <button
+              class="btn btn-outline-secondary text-start dropdown-toggle w-100"
+              type="button"
+              data-bs-toggle="dropdown"
+            >
+              {{ selectedCuisine || 'All Cuisines' }}
+            </button>
+            <ul class="dropdown-menu mt-1 shadow-sm w-100">
+              <li>
+                <button
+                  class="dropdown-item w-100 text-start"
+                  :class="{ active: selectedCuisine === '' }"
+                  @click="selectedCuisine = ''"
+                >
+                  All Cuisines
+                </button>
+              </li>
 
-  <div v-if="isLoading" class="loading">Loading...</div>
-  <div v-if="error" class="error">{{ error }}</div>
+              <li v-for="cuisine in cuisines" :key="cuisine">
+                <button
+                  class="dropdown-item w-100 text-start"
+                  :class="{ active: selectedCuisine === cuisine }"
+                  @click="selectedCuisine = cuisine"
+                >
+                  {{ cuisine }}
+                </button>
+              </li>
+            </ul>
+          </div>
+        </div>
 
-  <div v-if="recipes.length > 0" class="recipes">
-    <RouterLink :to="`/recipe/${recipe.id}`" v-for="recipe in recipes" :key="recipe.id">
-      <img :src="recipe.image" alt="" />
-      {{ recipe.title }}
-    </RouterLink>
+        <div
+          class="col col-12 col-md-6 d-flex flex-column flex-md-row align-items-start align-items-md-center"
+        >
+          <input
+            v-model="query"
+            type="text"
+            name="search"
+            placeholder="Search recipe"
+            class="form-control me-md-3 mb-2 mb-md-0"
+            @keyup.enter="searchRecipes"
+          />
+          <button
+            class="btn btn-outline-secondary w-100 w-md-auto"
+            @click="searchRecipes"
+            :disabled="isLoading"
+          >
+            Search
+          </button>
+        </div>
+      </div>
 
-    <div class="pagination">
-      <button @click="changePage(currentPage - 1)" :disabled="currentPage === 1">Previous</button>
-      <span>Page {{ currentPage }} of {{ totalPages }}</span>
-      <button @click="changePage(currentPage + 1)" :disabled="currentPage === totalPages">
-        Next
-      </button>
+      <div v-if="isLoading && !error" class="d-flex justify-content-center my-5">
+        <div class="spinner-border text-secondary" role="status" style="width: 3rem; height: 3rem">
+          <span class="visually-hidden">Loading...</span>
+        </div>
+      </div>
+      <div v-if="error" class="error">{{ error }}</div>
+
+      <div v-if="recipes.length > 0" class="recipes">
+        <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3 mb-3">
+          <RouterLink :to="`/recipe/${recipe.id}`" v-for="recipe in recipes" :key="recipe.id">
+            <div class="col mb-3 mb-sm-0 h-100">
+              <div class="card h-100">
+                <img :src="recipe.image" class="card-img-top" :alt="recipe.title" />
+                <div class="card-body">
+                  <h4>{{ recipe.title }}</h4>
+                </div>
+              </div>
+            </div>
+          </RouterLink>
+        </div>
+
+        <nav aria-label="Page navigation" class="d-flex justify-content-center mt-4">
+          <ul class="pagination">
+            <li class="page-item" :class="{ disabled: currentPage === 1 }">
+              <button
+                class="page-link"
+                @click="changePage(currentPage - 1)"
+                :disabled="currentPage === 1"
+              >
+                Previous
+              </button>
+            </li>
+
+            <li class="page-item disabled">
+              <span class="page-link"> Page {{ currentPage }} of {{ totalPages }} </span>
+            </li>
+
+            <li class="page-item" :class="{ disabled: currentPage === totalPages }">
+              <button
+                class="page-link"
+                @click="changePage(currentPage + 1)"
+                :disabled="currentPage === totalPages"
+              >
+                Next
+              </button>
+            </li>
+          </ul>
+        </nav>
+      </div>
+      <div v-else-if="!isLoading && hasSearched">
+        <span>No recipes found.</span>
+      </div>
     </div>
-  </div>
-  <div v-else-if="!isLoading">
-    <span>No recipes found.</span>
   </div>
 </template>
 
